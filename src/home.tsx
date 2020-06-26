dc13af41138ad9bdcd7ed4bf236a0b6bfa25b51f
Sachin Shapur <sachin.shapur@intimetec.com>
2020-06-26 17:49:17 +0530

    added basic structure of the project

---
+++ src/home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Button} from 'react-bootstrap'
+
+const Home : React.FC = () => {
+    return (<div>
+        <Button  variant="primary" size="lg" type="submit">ADD Complaint</Button>
+        </div>
+)}
+
+export default Home;
